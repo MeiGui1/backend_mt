d31eddd4ab95d95219e49a0041ae8c243b48f5f5
MeiGui1 <alice_truong95@hotmail.com>
2019-07-23 14:22:12 +0200

    fix foreignkey bug

--- ConsApp/src/main/resources/db/migration/V4__ExerciseTables.sql
+++ ConsApp/src/main/resources/db/migration/V4__ExerciseTables.sql
@@ -27,13 +27,13 @@ INSERT INTO ExerciseType (title, explanation) VALUES ('MassageWange','Durch Zäh
 INSERT INTO ExerciseType (title, explanation) VALUES ('Medikamentenpflaster','Über Nacht sind selbsthaftende Pflaster mit entzündungshemmenden Wirkstoffen zur Schmerzbehandlung geeignet.');
 
 
-INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('1', 'Übung1');
-INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('1', 'Übung4');
-INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('2', 'Übung2');
-INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('3', 'Übung2');
-INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('3', 'Übung5');
-INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('4', 'Übung1');
-INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('5', 'Übung1');
-INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('6', 'Übung3');
-INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('6', 'Übung2');
+INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('1', 'Selbstbeobachtung');
+INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('1', 'MassageWange');
+INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('2', 'Muskeldehnung');
+INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('3', 'Muskeldehnung');
+INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('3', 'Medikamentenpflaster');
+INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('4', 'Selbstbeobachtung');
+INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('5', 'Selbstbeobachtung');
+INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('6', 'MassageSchlaefe');
+INSERT INTO PatientExercise (patient_id, exercisetype_title) VALUES ('6', 'Muskeldehnung');
 
